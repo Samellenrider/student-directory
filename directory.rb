0877fd13d32c4729dc807c20ed5b97bb00f0627e
Sam Ellenrieder <sam.ell@web.de>
2017-07-17 12:55:17 +0100

    Add a list of students

---
+++ directory.rb
@@ -0,0 +1,15 @@
+puts
+puts "The students of Villians Academy"
+puts "_____________"
+puts "Dr. Hanniobal Lecter"
+puts "DArth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+puts
