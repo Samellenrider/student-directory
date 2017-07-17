3ae346df28cc31af03e96aca9f0be6e51e3a1d72
Sam Ellenrieder <sam.ell@web.de>
2017-07-17 14:57:21 +0100

    Adding students information

--- directory.rb
+++ directory.rb
@@ -1,20 +1,22 @@
 
 
-puts # I like empty lines in my code. It makes it look more open
+puts # I like empty lines in my code. It makes it look more open # we store the cohort month as a symbol because we won't expect to treat is as a string
 # All of our students in one array
 students = [
-    "Dr. Hannibal  Lecter",
-    "Darth Vader",
-    "Nurse Ratched",
-    "Michael Corleone",
-    "Alex Delarge",
-    "The Wicked Witch of the West",
-    "Terminator",
-    "Freddy Krueger",
-    "The Joker",
-    "Joffrey Baratheon",
-    "Norman Bates",
-    "Patrick Bateman"
+
+{name: "Dr. Hannibal Lecter", cohort: :July},
+{name: "Darth Vader", cohort: :July},
+{name: "Nurse Ratched", cohort: :July},
+{name: "Michael Corleone", cohort: :July},
+{name: "Alex DeLarge", cohort: :July},
+{name: "The Wicked Witch of the West", cohort: :July},
+{name: "Termiator", cohort: :July},
+{name: "Freddy Krueger", cohort: :July},
+{name: "The Joker", cohort: :July},
+{name: "Joffrey Baratheon", cohort: :July},
+{name: "Norman Bates", cohort: :July},
+{name: "Patrick Bateman", cohort: :July}
+
 ]
 
 puts # Ups I did it again
@@ -27,27 +29,30 @@ end
 
 # We print them. Iteration helps
 
-def print(names)
+def print(students)
     
-    names.each do |name|
+    students.each do |student|
         
-        puts name
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+        
   end
 end
 
-def print_footer(names)
+def print_footer(students)
     
     
-    puts "Overall, we have #{names.count} great students."
+    puts "Overall, we have #{students.count} great students."
     
 end
 
-
-
 # We call the methods to print everything to the screen
-print_header
-print(students) # We are passing the students variable to the methods as an argument (names) because the methods don't have acces to local variables defined outside of them
+
+   print_header
+
+   print(students) # We are passing the students variable to the methods as an argument (names) because the methods don't have acces to local variables defined outside of them
+
 puts # And again
-print_footer(students)
+
+   print_footer(students)
 
 puts # And again
