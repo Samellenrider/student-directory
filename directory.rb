28a4a17ae05c195e4e2b9cde4fef8d4c7285fd4e
Sam Ellenrieder <sam.ell@web.de>
2017-07-17 14:12:37 +0100

    No repetition, Iteration and a new student

--- directory.rb
+++ directory.rb
@@ -1,23 +1,38 @@
-student_count = 11
+
 
 puts # I like empty lines in my code. It makes it look more open
-puts "The students of Villians Academy"
-puts "_____________"
-puts "Dr. Hanniobal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+# All of our students in one array
+students = [
+    "Dr. Hannibal  Lecter",
+    "Darth Vader",
+    "Nurse Ratched",
+    "Michael Corleone",
+    "Alex Delarge",
+    "The Wicked Witch of the West",
+    "Terminator",
+    "Freddy Krueger",
+    "The Joker",
+    "Joffrey Baratheon",
+    "Norman Bates",
+    "Patrick Bateman"
+]
 
 puts # Ups I did it again
 
-puts "Overall, we have #{student_count} great students "
+puts "Students of Villians Academy"
+puts "_____________"
+# We print them. Iteration helps
+
+students.each do |student|
+
+    puts student
+
+end
 
 puts # And again
+
+# We print the total
+
+puts "Overall, we have #{students.count} great students. "
+
 puts # And again
