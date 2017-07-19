b5492a94e8648302cf051e923dafcf6ecbed0794
Sam Ellenrieder <sam.ell@web.de>
2017-07-19 13:09:23 +0100

    specific letter

--- directory.rb
+++ directory.rb
@@ -1,28 +1,33 @@
 puts # I like empty lines in my code. It makes it look more open
 
 
+
+
+
 def input_students
     
     puts "Please enter the name of the students"
     
     puts "to finish, just hit return twice"
     
-# Create an empty array
-    students = []
+    students = []  # Create an empty array
     
 # Get the first name
     name = gets.chomp
     
 # While the name is filled, repeat this code
-    while name.start_with?('S') do
+    while !name.empty? do
         # Add the hash to the array
         students << {name: name, cohort: :July}
         
-        puts "Now we have #{students.count} students"
         
+        puts "Now we have #{students.count} students"
+    
+    
+    
 # Get another name from the user
         name = gets.chomp
-    end
+  end
 # Return the array of students
     students
 end
@@ -41,16 +46,26 @@ end
 def print(students)
     
     students.each_with_index do |student, index|
+        
+        if student[:name].start_with?('S')
     
-         puts "#{index + 1}#{':'} #{student[:name]} (#{student[:cohort]} cohort) "
+      puts "#{index + 1}#{':'} #{student[:name]} (#{student[:cohort]} cohort) "
         
   end
+ end
 end
 
 def print_footer(students)
     
-    puts "Overall, we have #{students.count} great students."
+    if students == 1
+       
+       puts "overall, we have #{students.count} great student."
     
+    else
+    
+    
+       puts "Overall, we have #{students.count} great students."
+  end
 end
 
 # We call the methods to print everything to the screen
@@ -66,4 +81,5 @@ puts # And again
    print_footer(students)
 
 puts # And again
+
 
